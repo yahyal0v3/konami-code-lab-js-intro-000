70038ebbb23135ee4240fb1a2cff79e0fdd105d5
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-06-29 02:58:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e) {
+    let index = 0
+    if (e.key === codes[index]) {      
+      index++
+      console.log(index)
+      console.log(e.key)
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 }
