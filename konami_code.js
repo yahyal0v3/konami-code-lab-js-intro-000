1e7def42772c7a7cd46aa711c6cf4a9c6b7f8a0e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-06-29 03:03:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === codes[index]) {
+      index++
+      console.log(e.key)
+      console.log(index)
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
+
 }
